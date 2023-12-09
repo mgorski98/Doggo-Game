263ac381450f15c212abfeb24a37606b67a65d4b
BigDinDonMan <gorski.maciej@o2.pl>
2023-12-09 14:20:28 +0100

    implemented basic music player

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using Sirenix.OdinInspector;
+
+public class MusicPlayer : SerializedMonoBehaviour
+{
+    public AudioClip[] MusicClips;
+    public AudioSource ASource;
+    public AudioMixerGroup MusicGroup;
+    public AudioMixer Mixer;
+    public string MusicVolumeProperty;
+
+    private int CurrentIndex = 0;
+    private Coroutine FadeOutCoro;
+
+    [Header("CONFIG")]
+    public float FadeInDurationSeconds = 2f;
+    [Range(0.9f, 1f)]
+    public float ProgressFadeOutThreshold = 0.9f;
+    public float MusicVolume;
+    public bool PlayRandom = false;
+
+    public const float MIN_VOLUME = 0.0001f;
+
+    private void Awake()
+    {
+        if (MusicClips.Length <= 0) {
+            enabled = false;
+            return;
+        }
+        Mixer.GetFloat(MusicVolumeProperty, out MusicVolume);
+        MusicVolume = MusicVolume.FromDecibels();
+    }
+
+    private void Update() {
+        if (MusicClips.Length <= 0)
+            return;
+
+        if (ASource.isPlaying) {
+            var progress = ASource.time / ASource.clip.length;
+            if (progress >= ProgressFadeOutThreshold && FadeOutCoro == null) {
+                float timeLeft = ASource.clip.length - ASource.time;
+                FadeOutCoro = StartCoroutine(FadeOutCurrentSound(timeLeft));
+            }
+        }
+
+        if (!ASource.isPlaying) {
+            NextSong(true);
+        }
+    }
+
+    public void NextSong(bool fadeIn) {
+        CurrentIndex = PlayRandom ? Random.Range(0, MusicClips.Length) : Utils.ClampWrapping(CurrentIndex + 1, 0, MusicClips.Length - 1);
+        ASource.clip = MusicClips[CurrentIndex];
+        ASource.Play();
+        if (fadeIn)
+            StartCoroutine(FadeInCurrentSound(FadeInDurationSeconds));
+    }
+
+    private IEnumerator FadeOutCurrentSound(float timeLeft) {
+        for (float t = timeLeft; t > 0; t -= Time.deltaTime) {
+            var progress = 1f - (t / timeLeft);
+            var interpValue = Mathf.Lerp(MusicVolume, MIN_VOLUME, progress);
+            var newVol = interpValue.ToDecibels();
+            Mixer.SetFloat(MusicVolumeProperty, newVol);
+            yield return null;
+        }
+        FadeOutCoro = null;
+    }
+
+    private IEnumerator FadeInCurrentSound(float duration) {
+        for (float t = 0; t < duration; t += Time.deltaTime) {
+            var interpValue = Mathf.Lerp(MIN_VOLUME, MusicVolume, t / duration);
+            var newVol = interpValue.ToDecibels();
+            Mixer.SetFloat(MusicVolumeProperty, newVol);
+            yield return null;
+        }
+    }
+}
--- Assets/Scripts/UIHelpers.cs
+++ Assets/Scripts/UIHelpers.cs
@@ -1,12 +1,15 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.Localization;
+using UnityEngine.Audio;
 
 public class UIHelpers : MonoBehaviour
 {
     public DoggoConfirmationDialog Dialog;
     public LocalizedString ExitGameText;
     public LocalizedString ExitToMenuText;
+
+    public AudioMixer Mixer;
 
     public void ShowExitGameDialog() {
         if (PlayerProgress.Instance.GameOver)
@@ -29,4 +32,13 @@ public class UIHelpers : MonoBehaviour
     }
 
     public void ToggleRetroMode() => GameModifiers.Instance.RetroMode = !GameModifiers.Instance.RetroMode;
+
+    public void ToggleMusic() {
+        //Mixer.SetFloat("MusicVolume", Mathf.Log10(0.0001f) * 20);
+    }
+
+    public void ToggleSounds() {
+        //Mixer.SetFloat("SoundVolume", Mathf.Log10(0.0001f) * 20);
+    }
 }
+
