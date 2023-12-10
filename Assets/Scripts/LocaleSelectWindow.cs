a261d87befd7ea22eba5681cdb79f40efefd82eb
BigDinDonMan <gorski.maciej@o2.pl>
2023-12-10 15:08:50 +0100

    added boot scene, new icons and finished locale select

--- Assets/Scripts/LocaleSelectWindow.cs
+++ Assets/Scripts/LocaleSelectWindow.cs
@@ -24,21 +24,21 @@ public class LocaleSelectWindow : SerializedMonoBehaviour
 
     private void Awake() {
         var locales = LocalizationSettings.AvailableLocales.Locales;
-
         for (int i = 0; i < locales.Count; ++i) {
             var obj = Instantiate(SelectionButtonPrefab, SelectionButtonsParent);
             var button = obj.GetComponent<ToggleButton>();
+            var id = i;
             ToggleGroup.Add(button);
             button.onClick.AddListener(() => {
-                LocaleID = i;
+                LocaleID = id;
             });
             obj.GetComponent<Image>().sprite = LocaleIcons[locales[i].Identifier.ToString()];
         }
     }
 
     public void ConfirmSelection() {
+        PlayerPrefs.SetString("LANGUAGE_ID", LocalizationSettings.AvailableLocales.Locales[LocaleID].Identifier.ToString());
         I18n.Instance.ChangeLocale(LocaleID);
-        //todo: store locale in playerprefs
     }
 
     private void Update() {
---
+++ Assets/Scripts/Startup.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEngine.Localization.Settings;
+using UnityEngine.Localization;
+using UnityEngine.SceneManagement;
+
+
+public class Startup : MonoBehaviour {
+    private void Awake() {
+        var selectedLang = PlayerPrefs.GetString("LANGUAGE_ID", "English(en)");
+        var index = 0;
+        for (int i = 0; i < LocalizationSettings.AvailableLocales.Locales.Count; ++i) {
+            var locale = LocalizationSettings.AvailableLocales.Locales[i];
+            if (locale.Identifier.ToString() == selectedLang) {
+                index = i;
+                break;
+            }
+        }
+        I18n.Instance.ChangeLocale(index);
+    }
+
+    private void Update() {
+        if (!I18n.Instance.ChangeInProgress) {
+            SceneManager.LoadScene("MenuScene");
+        }
+    }
+}
